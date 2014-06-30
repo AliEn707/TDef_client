447d719f586668a288852f2aa23107643334c294
AliEn707 <1alien707@qip.ru>
2014-06-30 23:56:36 +0400

    not tested

--- file.c
+++ file.c
@@ -23,10 +23,64 @@ void loadMenu(char* path){
 				m=&m->submenu[ctoi(buf[i])];
 			continue;
 		}
-		if (strcmp(buf,"objects")==0){
+		if (strcmp(buf,"submenu")==0){
 			fscanf(file,"%d\n",&m->submenu_size);
 			if ((m->submenu=malloc(m->submenu_size*sizeof(menu)))==0)
-				perror("malloc submenu");
+				perror("malloc submenu loadMenu");
+		}
+		if (strcmp(buf,"objects")==0){
+			fscanf(file,"%d\n",&m->objects_size);
+			if ((m->objects=malloc(m->objects_size*sizeof(object)))==0)
+				perror("malloc objects loadMenu");
+			int i=-1;
+			while(i<m->object_size){
+				fscanf(file,"%s ",buf);
+				if(strcmp(buf,"//")==0){
+					i++;
+				}
+				if(strcmp(buf,"xpos")==0){
+					char c;
+					fscanf(file,"%c %d\n",&c,&);
+				}
+				//something else
+				if(strcmp(buf,"elements")==0){
+					#define element m->objects[i].elements[j]
+					fscanf(file,"%d\n",&m->objects[i].elements_size);
+					if ((m->objects[i].elements=malloc(m->objects[i].elements_size*sizeof(element)))==0)
+						perror("malloc elements loadMenu");
+					int j=-1;
+					while(j<m->objects[i].elements_size){
+						fscanf(file,"%s ",buf);
+						if(strcmp(buf,"//")==0){
+							j++;
+							continue;
+						}
+						if(strcmp(buf,"color")==0){
+							fscanf(file,"%f %f %f\n",&element.color.r,&element.color.g,&element.color.b);
+						}
+						if(strcmp(buf,"xpos")==0){
+							fscanf(file,"%d\n",&element.position.x);
+						}
+						if(strcmp(buf,"ypos")==0){
+							fscanf(file,"%d\n",&element.position.y);
+						}
+						if(strcmp(buf,"xsize")==0){
+							fscanf(file,"%d\n",&element.size.x);
+						}
+						if(strcmp(buf,"ysize")==0){
+							fscanf(file,"%d\n",&element.size.y);
+						}
+						if(strcmp(buf,"tex")==0){
+							fscanf(file,"%s\n",&buf);
+							if (buf[0]=='0')
+								continue;
+							//add tex load
+						}
+						//somesing else
+					}
+					#undef element
+				}
+			}
 		}
 	}
 	
