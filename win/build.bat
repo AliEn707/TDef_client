9a8f3d0fd577f437df3cc9b771697c19f66eecdf
AliEn707 <1alien707@qip.ru>
2014-10-11 23:19:08 +0400

    add libs
    need to check fonts on keyboard
    start to add public part
    next must be rooms menu (find, and create)

--- win/build.bat
+++ win/build.bat
@@ -3,5 +3,5 @@
 @set PATH=%root%\MinGW\bin;%PATH%
 
 
-mingw32-gcc *.c ../*.c ../*.cpp -lSDL2main -lSDL2 -lSDL2_net -lopengl32 -lglu32 -lwinmm -lws2_32 -lstdc++ -Wall -g -o TDef 
+mingw32-gcc *.c ../*.c ../*.cpp -lSDL2main -lSDL2 -lSDL2_net -lopengl32 -lglu32 -lwinmm -lws2_32 -lstdc++  -Wall -g -o TDef 
 pause
